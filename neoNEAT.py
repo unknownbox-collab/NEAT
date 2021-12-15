6a68810ab60dab4ee4eb55819aeee6dc6c216b70
min <ekdmsdlfma@gmail.com>
2021-12-15 09:36:00 +0900

    bug fixed

--- neoNEAT.py
+++ neoNEAT.py
@@ -29,45 +29,48 @@ class Topology:
     def init(self,*edges):
         self.maxLayer = 0
         self.nodes = {}
-        for i in range(self.inNodeNum):
-            self.nodes[i] = Node(i,[],[],0)
-        for i in range(self.inNodeNum,self.outNodeNum + self.inNodeNum):
-            self.nodes[i] = Node(i,[],[],-1)
         self.edges = list(edges)
 
         def modifyLayer(nextNode):
             for edge in nextNode.next:
                 end = edge.end
-                if self.nodes[end].layer != -1 and self.nodes[end].layer < nextNode.layer + 1:
+                if self.nodes[end].layer != -1 and self.nodes[end].layer <= nextNode.layer + 1:
                     self.nodes[end].layer = nextNode.layer + 1
-                    if self.maxLayer < nextNode.layer + 1: self.maxLayer = nextNode.layer + 1
                     nextNode = self.nodes[end]
                     modifyLayer(nextNode)
                 else:
                     break
-
+        #print(f'=== edges : {list(map(lambda x: f"{x.start} -> {x.end}",self.edges))}')
         for edge in edges:
             if not edge.start in self.nodes.keys():
-                self.nodes[edge.start] = Node(edge.start,[],[edge],1)
-                if self.maxLayer < 1: self.maxLayer = 1
+                self.nodes[edge.start] = Node(edge.start,[],[edge],0)
             else:
                 self.nodes[edge.start].next.append(edge)
+
             if not edge.end in self.nodes.keys():
                 frontLayer = self.nodes[edge.start].layer
                 self.nodes[edge.end] = Node(edge.end,[edge],[],frontLayer + 1)
                 if self.inNodeNum <= edge.end < self.inNodeNum + self.outNodeNum:
                     self.nodes[edge.end].layer = -1
-                if self.maxLayer < frontLayer + 1:
-                    self.maxLayer = frontLayer + 1
             else:
                 self.nodes[edge.end].before.append(edge)
                 frontLayer = self.nodes[edge.start].layer
-                if self.nodes[edge.end].layer < frontLayer + 1 and self.nodes[edge.end].layer != -1:
+                if (self.nodes[edge.end].layer < frontLayer + 1 and self.nodes[edge.end].layer != -1):
                     self.nodes[edge.end].layer = frontLayer + 1
                     modifyLayer(self.nodes[edge.end])
+        #print(f'=== nodes : {list(map(lambda x: f"{x} : {self.nodes[x].layer}",self.nodes.keys()))}')
+        
+        for i in range(self.inNodeNum):
+            if not i in self.nodes.keys():
+                self.nodes[i] = Node(i,[],[],0)
+        for i in range(self.inNodeNum,self.outNodeNum + self.inNodeNum):
+            if not i in self.nodes.keys():
+                self.nodes[i] = Node(i,[],[],-1)
     
     def forward(self,*inputValue):
         values = {}
+        print("====FORWARD====")
+        self.maxLayer = max(map(lambda x: self.nodes[x].layer,self.nodes.keys()))
         for nodeId in self.nodes.keys():
             values[nodeId] = 0
             if nodeId < self.inNodeNum :
@@ -80,14 +83,14 @@ class Topology:
             targetLayer = nowLayer + 1
             if nowLayer == self.maxLayer:
                 targetLayer = -1
-            print("taget :",targetLayer)
+            print("- taget :",targetLayer)
             for edge in self.edges:
                 if self.nodes[edge.end].layer == targetLayer and not edge.disAbled:
                     if self.nodes[edge.start].layer != 0:
                         values[edge.end] += ReLU(values[edge.start] * edge.weight)
                     else:
                         values[edge.end] += values[edge.start] * edge.weight
-                    print("forward :",edge.start,edge.end,values[edge.end])
+                    print(f"-- forward : {edge.start} -> {edge.end}[{values[edge.end]}]")
         return [values[i] for i in range(self.inNodeNum , self.inNodeNum + self.outNodeNum)]
 
     def addEdgeMutation(self):
@@ -123,30 +126,16 @@ class Topology:
     def addNodeMutation(self):
         global innovNum
 
-        def modifyLayer(nextNode):
-            for edge in nextNode.next:
-                end = edge.end
-                if self.nodes[end].layer != -1 and self.nodes[end].layer <= nextNode.layer + 1:
-                    self.nodes[end].layer = nextNode.layer + 1
-                    if self.maxLayer <= nextNode.layer + 1: self.maxLayer = nextNode.layer + 1
-                    nextNode = self.nodes[end]
-                    modifyLayer(nextNode)
-                else:
-                    break
-
         edgeNum = random.randint(0,len(self.edges)-1)
         edge = self.edges[edgeNum]
+        while self.edges[edgeNum].disAbled:
+            edgeNum = random.randint(0,len(self.edges)-1)
+            edge = self.edges[edgeNum]
         self.edges[edgeNum].disAbled = True
 
         newId = max(self.nodes.keys()) + 1
         frontEdge = Edge(edge.start, newId, 1.0, innovNum)
         endEdge = Edge(newId, edge.end, edge.weight, innovNum)
-
-        self.nodes[newId] = Node(newId, [frontEdge], [endEdge], self.nodes[edge.start].layer + 1)
-        if self.nodes[newId].layer >= self.nodes[edge.end].layer:
-            nextNode = self.nodes[newId]
-            if self.maxLayer < nextNode.layer + 1: self.maxLayer = nextNode.layer
-            modifyLayer(nextNode)
 
         self.nodes[edge.start].next.append(frontEdge)
         innovNum += 1
@@ -156,8 +145,8 @@ class Topology:
 
         self.edges.append(frontEdge)
         self.edges.append(endEdge)
-
-        print(f'newNode({newId}) : {edge.start} -> {edge.end}')
+        self.init(*self.edges)
+        print(f'>> newNode : {edge.start} -> ({newId}) -> {edge.end}')
 
 t = Topology(3,2)
 innovNum = 5
@@ -165,11 +154,10 @@ t.init()
 t.addEdgeMutation()
 t.addNodeMutation()
 t.addEdgeMutation()
+t.addNodeMutation()
 t.addEdgeMutation()
 t.addEdgeMutation()
 t.addEdgeMutation()
 t.addEdgeMutation()
-#for i in t.nodes.keys():
-#    print(f'{i} : {t.nodes[i].before} // {t.nodes[i].next} // {t.nodes[i].layer}')
-result = t.forward(2,2,2)
+result = t.forward(1,1,1)
 print(result)
